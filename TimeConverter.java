20a4bd663eb5ac0beb5b8f9250175339ca34d6a0
siddamshettyManeesha <137407991+siddamshettyManeesha@users.noreply.github.com>
2025-04-30 18:52:11 +0530

    Add files via upload

---
+++ TimeConverter.java
@@ -0,0 +1,8 @@
+public class TimeConverter{
+public static int hoursToMinutes(int hours){
+return hours*60;
+}
+public static void main(String [] args){
+System.out.println(TimeConverter.hoursToMinutes(2));
+}
+} 
